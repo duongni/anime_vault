9f51914e6883acb42cc27380171dc03f91e56df1
duongni <133175915+duongni@users.noreply.github.com>
2024-01-30 23:51:02 -0500

    apply framer motion to the app

--- app/action.tsx
+++ app/action.tsx
@@ -1,4 +1,5 @@
 "use server";
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
 
 export const fetchAnime = async (page: number) => {
   const response = await fetch(
@@ -7,5 +8,7 @@ export const fetchAnime = async (page: number) => {
 
   const data = await response.json();
 
-  return data;
+  return data.map((item: AnimeProp, index: number) => (
+    <AnimeCard key={item.id} anime={item} index={index} />
+  ));
 };
